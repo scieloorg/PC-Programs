267d89e9db7d59173f45f332222a72d91399d29e
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2010-04-05 15:24:54 +0000

    Markup PMC - reorganizando as pastas do módulo PMC (XSL, DTD, etc) Markup PMC - caminho da DTD PMC local configurável

--- src/scielo/bin/cfg/configure_paths.bat
+++ src/scielo/bin/cfg/configure_paths.bat
@@ -5,10 +5,18 @@ if exist %1\bin\scielo_paths.ini copy %1\bin\scielo_paths.ini %1\bin\scielo_path
 
 %1\bin\cfg\mx seq=%1\bin\cfg\temp.txt lw=99999 "pft=replace(v1,'C:\SCIELO','%1'),#" now >  %1\bin\sgmlpars\settings.cfg
 
+
+%1\bin\cfg\mx null count=1 "pft='`d9999a9999{',replace('%x%','\','/'),'{`'" now > x.txt
+
+%1\bin\cfg\mx seq=%1\bin\markup\pmc\v3.0\sgml2xml\sgml2pmc4local.original.xsl lw=99999 "proc=@x.txt" "pft=replace(v1,'c:\programas\scielo',v9999),#" now > %1\bin\markup\pmc\v3.0\sgml2xml\sgml2pmc4localvalidation.xsl
+
+
 if exist %2\serial\code\code.fst xcopy /K /Q /Y /O  %2\serial\code\code.fst %2\serial\code\newcode.fst
 
 if exist %2\serial\code\newcode.mst goto FIM
 if exist %2\serial\code\code.mst xcopy /K /Q /Y /O %2\serial\code\code.* %2\serial\code\newcode.*
+
+
 
 
 :FIM
--- src/scielo/bin/markup_pmc/pmc/v3.0/sgml2xml/sgml2pmc4local.original.xsl
+++ src/scielo/bin/markup_pmc/pmc/v3.0/sgml2xml/sgml2pmc4local.original.xsl
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:util="http://dtd.nlm.nih.gov/xsl/util" xmlns:mml="http://www.w3.org/1998/Math/MathML" exclude-result-prefixes="util xsl">
-	<xsl:output encoding="utf-8" method="xml" indent="yes" doctype-public="SYSTEM" doctype-system="file://../dtd/publishing/journalpublishing3.dtd"/>
+	<xsl:output encoding="utf-8" method="xml" indent="yes" doctype-public="SYSTEM" doctype-system="file://c:\programas\scielo/bin/markup/dtd/publishing/journalpublishing3.dtd"/>
 
 	<xsl:include href="sgml2generic.xsl"/>
 	<xsl:include href="sgml2generic_pmc.xsl"/>
