3e7dbce86a28adb2b2968a3641baf52878d5799a
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2010-07-12 20:13:35 +0000

    PC Programs desenvolvimento de gerador de documentação da base artigo

--- src/scielo/bin/config/help/genConverterDoc.bat
+++ src/scielo/bin/config/help/genConverterDoc.bat
@@ -1,13 +1,22 @@
 mx ..\..\convert\library\scielo\artmodel lw=99999 "pft=@genConverterArtmodel.pft" now | sort > genConverter_artmodel.seq
-mx seq=genConverter_artmodel.seq create=genConverter_artmodel  now -all fst=@ fullinv=genConverter_artmodel
+mx seq=genConverter_artmodel.seq create=genConverter_artmodel  now -all fst=@genConverter_artmodel.fst fullinv=genConverter_artmodel
 
 
-mx "seq=..\..\convert\library\scielo\article.2db" create=genConverter_art2db now -all 
-mx genConverter_art2db lw=9999 "pft=v1/" now | sort > article.2db
-mx "seq=article.2db;" create=genConverter_art2db now -all  "fst=1 0 v1/" fullinv=genConverter_art2db
+rem mx "seq=..\..\convert\library\scielo\article.2db" create=genConverter_art2db now -all 
+rem mx genConverter_art2db lw=9999 "pft=v1/" now | sort > article.2db
+rem mx "seq=article.2db;" create=genConverter_art2db now -all  "fst=1 0 v1/" fullinv=genConverter_art2db
+
  
-mx "seq=..\..\convert\library\scielo\article.trl," "pft=@genConverter_transl.pft" now > genConverter_transl.xml
-echo "</root>"  >> genConverter_transl.xml
+mx "seq=..\..\convert\library\scielo\article.trl,"  lw=9999  "pft=@genConverter_transl.pft" now > genConverter.xml
+mx null count=1 "pft='</transl>'"  >> genConverter.xml
+
+mx "seq=..\..\convert\library\scielo\article.2db;"  lw=9999 "pft=@genConverter_art2db.pft" now >> genConverter.xml
+mx null count=1 "pft='</art2db>'"  >> genConverter.xml
+
+mx genConverter_artmodel lw=9999 "pft=@genConverter_artmodel.pft" now >> genConverter.xml
+mx null count=1 "pft='</artmodel>'"  >> genConverter.xml
+
+mx null count=1 "pft='</root>'"  >> genConverter.xml
 
 rem 1 tag
 rem 2 tag/subc
