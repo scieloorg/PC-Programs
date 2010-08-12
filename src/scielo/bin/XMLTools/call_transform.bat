5b3d1144dec691203b31c780ec877d8bd609fb43
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2010-08-12 12:03:31 +0000

    Markup PMC correção de geração de XML e melhoria na identificação de seções

--- src/scielo/bin/XMLTools/call_transform.bat
+++ src/scielo/bin/XMLTools/call_transform.bat
@@ -3,24 +3,14 @@ set XML_TOOLS_PATH=%2
 set xml_filename=%3
 set xsl_filename=%4
 set transformation_result_filename=%5
+set error_filename=%6
 
 if exist %transformation_result_filename%.tmp del %transformation_result_filename%.tmp
 if exist %transformation_result_filename% del %transformation_result_filename%
+if exist %error_filename% del %error_filename%
 
 %JAVA_EXE% -jar %XML_TOOLS_PATH%\core\saxon8.jar -novw -w0 -o %transformation_result_filename%.tmp %xml_filename% %xsl_filename% 
+
 if exist %transformation_result_filename%.tmp copy %transformation_result_filename%.tmp %transformation_result_filename%
-if not exist %transformation_result_filename%.tmp echo error > %transformation_result_filename%.err
+if not exist %transformation_result_filename%.tmp echo error > %error_filename%
 
-goto END
-
-rem %JAVA_EXE% -jar %XML_TOOLS_PATH%\core\saxon8.jar -novw -w0 -o d:\temp\teste.result.xml %xml_filename% %xsl_filename%  > d:\temp\teste.msg.txt
-rem dir d:\temp\
-
-
-%JAVA_EXE% -jar %XML_TOOLS_PATH%\core\saxon8.jar -novw -w0 %xml_filename% %xsl_filename% >  %transformation_result_filename%.tmp
-
-if not exist %transformation_result_filename%.tmp echo error > %transformation_result_filename% 
-if exist %transformation_result_filename%.tmp copy %transformation_result_filename%.tmp %transformation_result_filename%
-if exist %transformation_result_filename%.tmp del %transformation_result_filename%.tmp
-
-:END
