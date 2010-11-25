06f84e1a4c43ec4393834180755f55d20a4caa32
roberta.takenaka <roberta.takenaka@7b3c32f5-e272-4d6f-9936-548b95b19007>
2010-11-25 19:58:53 +0000

    pc_programs: script de exportação de serial

--- src/scielo/export/scripts/ExportSerial_append.bat
+++ src/scielo/export/scripts/ExportSerial_append.bat
@@ -1,4 +1,4 @@
 cisis\mx %1\title\title text=%2 "proc=if v68='%2' then else 'd*' fi" append=temp\serial\title\title now -all
-cisis\mx %1\issue\issue text=%3 "proc=if v930='%3' then else 'd*' fi" append=temp\serial\issue\issue now -all
-cisis\mx %1\section\section text=%3 "proc=if v930='%3' then else 'd*' fi" append=temp\serial\section\section now -all
+cisis\mx %1\issue\issue text=%3 "proc=if s(mpu,v930,mpl)='%3' then else 'd*' fi" append=temp\serial\issue\issue now -all
+cisis\mx %1\section\section text=%3 "proc=if s(mpu,v930,mpl)='%3' then else 'd*' fi" append=temp\serial\section\section now -all
 
