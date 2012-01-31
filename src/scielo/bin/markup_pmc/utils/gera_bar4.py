6c36d53ee823bd158e5fe9cac2a32227a23770aa
Roberta Takenaka <roberta.takenaka@scielo.org>
2012-01-31 17:59:45 -0200

    traducao dos textos das barras

--- src/scielo/bin/markup_pmc/utils/gera_bar4.py
+++ src/scielo/bin/markup_pmc/utils/gera_bar4.py
@@ -1,10 +1,12 @@
-
+# -*- coding: iso-8859-1 -*-
 class Bar:
     def __init__(self):
         self.d = {}
         self.order = []
         self.names = {}
         self.texts = {}
+        
+        
     def organize_semantically(self):
     	for k,item in self.names.items():
     	    for k2,item2 in item.items():
@@ -14,7 +16,21 @@ class Bar:
     	for k,item in self.texts.items():
     	    for item2 in item:
     	        print k + ';'+ item2 
-    	
+    	        
+    def load_translations(self, translations_file):
+        f = open(translations_file, 'r')
+        content = f.readlines()
+        f.close()
+        self.tr = {}
+        self.tr_order = []
+        for line in content:
+            s = str(line)
+            s = s.replace('\r','' ).replace('\n','')
+            
+            part = s.split('|')
+            self.tr[ part[0] ] = part
+            self.tr_order.append(part[0])
+        
     def load_bars(self, file_name, lang):
         f = open(file_name, 'r')
         content = f.readlines()
@@ -86,14 +102,33 @@ class Bar:
             print linha
             print '' 
             
+    def print_texts(self):
+    	for k,item in self.texts.items():
+    	    print k
+    	    print self.translate(k, 'en')
+    	    print self.translate(k, 'es')
+    	    print ''
+    	    
+    def translate(self, text, lang):
+    	index = 0
+    	if lang == 'en':
+    	    index = 1
+    	if lang == 'es':
+    	    index = 2
+    	t = text
+    	#print 'TRADUZA ' + t 
+    	    
+    	for order in self.tr_order:
+    	    #print order
+    	    #print self.tr[ order ][0] + ' => '+ self.tr[ order ][index]
+    	    t = t.replace(self.tr[ order ][0], self.tr[ order ][index])
+        
+        return t	
+                
 bar = Bar()
-
+bar.load_translations('words.txt')
 bar.load_bars('../pt_bars.mds','pt')
 bar.load_bars('../es_bars.mds','es')
 bar.load_bars('../en_bars.mds','en')
 
-bar.write()
-bar.organize_semantically()
-print '------'
-bar.organize_texts()
-        
+bar.print_texts()
