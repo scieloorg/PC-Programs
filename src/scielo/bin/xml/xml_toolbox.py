1e9da7239e8934b82f5a6f25eda6fdf65d85995c
Roberta Takenaka <roberta.takenaka@scielo.org>
2013-06-21 09:15:02 -0300

    XML Packages Maker converts entities to their corresponding characters

--- src/scielo/bin/xml/xml_toolbox.py
+++ src/scielo/bin/xml/xml_toolbox.py
@@ -6,8 +6,118 @@ import tempfile
 import reuse.xml.xml_java as xml_java
 import random
 
+
 xml_tree = None
 report = None
+
+
+class EntitiesTable:
+    def __init__(self, filename = 'entities'):
+        f = open(filename, 'r')
+        lines = f.readlines()
+        f.close()
+        
+        self.table_number2char = {}
+        self.table_named2char = {}
+
+        for line in lines:
+            values = line.replace("\n", "").split('|')
+            if len(values) != 5:
+                print(line)
+            else:
+                char, number_ent, named_ent, ign2, no_accent = values
+                
+                if self.is_valid_char(char) and self.is_valid_named(named_ent):
+                    #entity_char = named_ent.replace('&','').replace(';','')
+                    if not number_ent in self.table_number2char.keys():
+                        self.table_number2char[number_ent] = char
+                    if not named_ent in self.table_named2char.keys():
+                        self.table_number2char[named_ent] = char
+
+        
+                    
+    def is_valid_char(self, char):
+        r = False
+        if char != '':
+            if not char in [ '>', '<', '&']:
+                r = True
+        return  r
+
+    def is_valid_named(self, named):
+        r = False
+        if named != '':
+            if not named in [ '&gt;', '&lt;', '&amp;']:
+                r = True
+        return  r
+
+    
+    def ent2chr(self, ent):
+        r = ent
+        if ent in self.table_number2char.keys():
+            r = self.table_number2char[ent]
+        elif ent in self.table_named2char.keys():
+            r = self.table_named2char[ent]
+        return r
+    #def find_number_entities(self, content):
+    #    l = []
+    #    if '&#' in content and ';' in content:
+    #        p = content.find('&#')
+    #        ent = content[p:]
+    #        ent = ent[0:ent.find(';')+1]
+    #        if ent in self.table_number2char.keys():
+    #            l.append(ent)
+    #    return l
+        
+    #def chr2ent(self, content):
+
+    #    for k,v in self.table_char2number.items():
+    #        content = content.replace(k, v)
+    #    return content
+
+    #def ent2chr(self, content):
+    #    entities = self.find_number_entities(content)
+    #    for ent in entities:
+    #        content = content.replace(ent, self.table_number2char[ent])
+    #    return content
+    
+
+
+
+
+entities_table = EntitiesTable('reuse/encoding/entities')
+
+
+def handle_entities(content):
+    import HTMLParser
+    h = HTMLParser.HTMLParser()
+    try:
+        content = h.unescape(content)
+    except:
+        print('Unable to use h.unescape')
+        pass 
+
+
+    if '&' in content:
+        PREFIX_ENT = '#PREFIX_ENT#'
+        while '&' in content:
+
+            ent = content[content.find('&'):]
+            ent = ent[0:ent.find(';')+1]
+
+            char = entities_table.ent2chr(ent)
+            if char == ent:
+                content = content.replace(ent, ent.replace('&',  PREFIX_ENT))
+                f = open('unknown_ent.txt', 'a+')
+                f.write( char + '|' + ent + '|'  + ent + '||' + '\n')
+                f.close()
+            else:
+                content = content.replace(ent, char)
+            
+        content = content.replace(PREFIX_ENT, '&')        
+
+        
+
+    return content
 
 
 
@@ -660,24 +770,18 @@ class XMLPackagesChecker:
         for ent, new_ent in ents.items():
             content = content.replace(ent, new_ent)
 
-        import HTMLParser
-        h = HTMLParser.HTMLParser()
-        try:
-            content = h.unescape(content)
-        except:
-            pass 
-
+        content = handle_entities(content)
 
         for ent, new_ent in ents.items():
             content = content.replace(new_ent, ent)
 
         if content != c:
-            
+            print(len(content))
             f = open(filename, 'wb')
-            f.write(content.encode('utf-8'))
+            f.write(content)
             f.close()
 
-
+    
 
     def prepare_file(self, filename, err_filename, acron, alternative_id, report):
         new_name = ''
