d79b184db0f40e4c47bbf3a7e98878c0785e280f
Roberta Takenaka <roberta.takenaka@scielo.org>
2014-03-24 16:22:34 -0300

    O XML é gerado apenas da base isis, não usa o XML PMC. (retrocedido)

--- src/scielo/xml_scielo/proc/general/scripts/validateXML.bat
+++ src/scielo/xml_scielo/proc/general/scripts/validateXML.bat
@@ -11,15 +11,15 @@ set WARN_FILENAME=%6
 set validate=%7
 
 
-echo XXX> temp\validation_result
+echo finished> temp\validation_result
 
-%JAVA_EXE% -cp java\validador\XMLCheck.jar br.bireme.XMLCheck.XMLCheck  %filename% %validate% > temp\validation_result
+%JAVA_EXE% -cp %XML_SCIELO_PROGRAM_PATH%\..\bin\jar\XMLCheck.jar br.bireme.XMLCheck.XMLCheck  %filename% %validate% > temp\validation_result
 
-echo XXX>>temp\validation_result
+echo finished>>temp\validation_result
 
 
 %MX% seq=temp\validation_result create=temp\validation_result now -all
-
+%MX% temp\validation_result count=1 now
 
 %MX% temp\validation_result count=1 lw=9999 "pft=if instr(v1,'finished')=0 then 'call scripts\handle_validation_result.bat %filename% %1 %2 %ERROR_FILENAME% %WARN_FILENAME% ',v1,' %captura%',# fi" now > temp\handle_validation_result.bat
 %MX% temp\validation_result count=1 lw=9999 "pft=if instr(v1,'finished')>0 then '%acron% %issueid%'/ fi" now >> %OK_LIST%
