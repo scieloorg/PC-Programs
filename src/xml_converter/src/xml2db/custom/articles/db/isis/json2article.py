3efeb2331bcf4755f976ee6184177e28d16717a4
Roberta Takenaka <roberta.takenaka@scielo.org>
2014-03-27 12:19:03 -0300

    Normalizacao da estrutura de titulo de artigo para tratar o idioma

--- src/xml_converter/src/xml2db/custom/articles/db/isis/json2article.py
+++ src/xml_converter/src/xml2db/custom/articles/db/isis/json2article.py
@@ -701,9 +701,8 @@ class JSON_Article:
         self.normalize_illustrative_materials()
         self.normalize_affiliations()
         self.normalize_keywords()
-        
+
         self.json_data['f'] = self.json_normalizer.convert_value(self.json_data['f'], '71', 'doctopic')
-        
         self.json_data['f'] = self.json_normalizer.normalize_dates(self.json_data['f'], '111', '112', '111')
         self.json_data['f'] = self.json_normalizer.normalize_dates(self.json_data['f'], '113', '114', '113')
 
@@ -713,7 +712,7 @@ class JSON_Article:
         # ja esta normalizada self.json_data['f'] = self.json_normalizer.normalize_dates(self.json_data['f'], '64', '65', '64')
 
         self.json_data['h'] = self.json_normalizer.format_for_indexing(self.json_data['f'])
-        self.json_data['l'] = self.json_normalizer.format_for_indexing(self.json_data['h'])     
+        self.json_data['l'] = self.json_normalizer.format_for_indexing(self.json_data['h'])
         #self.json_data['h'] = self.json_data['f']
         #self.json_data['l'] = self.json_data['f']
     
