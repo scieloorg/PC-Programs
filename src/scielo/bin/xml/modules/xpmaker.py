1faee9a84b440812ec8921dc4f2984540496c83e
Roberta Takenaka <roberta.takenaka@scielo.org>
2014-09-01 12:43:08 -0300

    Preparando xml package maker para usar packtools

--- src/scielo/bin/xml/modules/xpmaker.py
+++ src/scielo/bin/xml/modules/xpmaker.py
@@ -1,10 +1,219 @@
 import os
 import shutil
+import tempfile
 from datetime import datetime
 
 from modules import article
 from modules import xml_utils
-from modules import sgml2xml
+from modules import java_xml_utils
+from modules import xpchecker
+
+
+DEFAULT_VERSION = '1.0'
+PMC_PATH = './../../pmc'
+XSL_SGML2XML = {}
+XSL_SGML2XML['3.0'] = PMC_PATH + '/v3.0/xsl/sgml2xml/sgml2xml.xsl'
+XSL_SGML2XML['1.0'] = PMC_PATH + '/j1.0/xsl/sgml2xml/sgml2xml.xsl'
+
+DTD_FILENAME = {}
+DTD_FILENAME['3.0'] = PMC_PATH + '/v3.0/dtd/journalpublishing3.dtd'
+DTD_FILENAME['1.0'] = PMC_PATH + '/j1.0/dtd/jats1.0/JATS-journalpublishing1.dtd'
+
+DOCTYPE = {}
+DOCTYPE['3.0'] = '<!DOCTYPE article PUBLIC "-//NLM//DTD Journal Publishing DTD v3.0 20080202//EN" "{DTD_FILENAME}">'
+DOCTYPE['1.0'] = '<!DOCTYPE article PUBLIC "-//NLM//DTD JATS (Z39.96) Journal Publishing DTD v1.0 20120330//EN" "{DTD_FILENAME}">'
+
+XSL_REPORT = {}
+XSL_REPORT['3.0'] = PMC_PATH + '/v3.0/xsl/nlm-style-4.6.6/style-reporter.xsl'
+XSL_REPORT['1.0'] = PMC_PATH + '/j1.0/xsl/nlm-style-5.4/style-reporter.xsl'
+
+XSL_REPORT_PREP = {}
+XSL_REPORT_PREP['3.0'] = PMC_PATH + '/v3.0/xsl/scielo-style/stylechecker.xsl'
+XSL_REPORT_PREP['1.0'] = PMC_PATH + '/j1.0/xsl/scielo-style/stylechecker.xsl'
+
+
+def doctype(version):
+    return DTD_FILENAME.get(version, DOCTYPE[DEFAULT_VERSION])
+
+
+def dtd_filename(version):
+    return DTD_FILENAME.get(version, DTD_FILENAME[DEFAULT_VERSION])
+
+
+def xsl_sgml2xml(version):
+    return XSL_SGML2XML.get(version, XSL_SGML2XML[DEFAULT_VERSION])
+
+
+def xsl_prep_report(version):
+    return XSL_REPORT_PREP.get(version, XSL_REPORT_PREP)
+
+
+def xsl_report(version):
+    return XSL_REPORT.get(version, XSL_REPORT)
+
+
+class XMLContent(object):
+
+    def __init__(self, content):
+        self.content = content
+
+    def fix_dtd_location(self, dtd_filename, doctype):
+        if not dtd_filename in self.content:
+            if not '<?xml ' in self.content:
+                self.content = '<?xml version="1.0" encoding="utf-8"?>\n' + self.content
+
+            if '<!DOCTYPE' in self.content:
+                old_doctype = self.content[self.content.find('<!DOCTYPE'):]
+                old_doctype = old_doctype[0:old_doctype.find('>')+1]
+                self.content = self.content.replace(old_doctype, '')
+            if not '<!DOCTYPE' in self.content:
+                self.content = self.content.replace('\n<article ', doctype.replace('{DTD_FILENAME}', dtd_filename) + '\n<article ')
+
+    def fix(self):
+        self.content = self.content[0:self.content.rfind('>')+1]
+        self.content = self.content[self.content.find('<'):]
+        self.content = self.content.replace(' '*2, ' '*1)
+        if xml_utils.is_xml_well_formed(self.content) is None:
+            self._fix_style_tags()
+        if xml_utils.is_xml_well_formed(self.content) is None:
+            self._fix_open_close()
+
+    def _fix_open_close(self):
+        changes = []
+        parts = self.content.split('>')
+        for s in parts:
+            if '<' in s:
+                if not '</' in s and not '<!--' in s and not '<?' in s:
+
+                    s = s[s.find('<')+1:]
+                    if ' ' in s and not '=' in s:
+                        test = s[s.find('<')+1:]
+                        changes.append(test)
+        for change in changes:
+            print(change)
+            self.content = self.content.replace('<' + test + '>', '[' + test + ']')
+
+    def _fix_style_tags(self):
+        rcontent = self.content
+        tags = ['italic', 'bold', 'sub', 'sup']
+        tag_list = []
+        for tag in tags:
+            rcontent = rcontent.replace('<' + tag.upper() + '>', '<' + tag + '>')
+            rcontent = rcontent.replace('</' + tag.upper() + '>', '</' + tag + '>')
+            tag_list.append('<' + tag + '>')
+            tag_list.append('</' + tag + '>')
+            rcontent = rcontent.replace('<' + tag + '>',  'BREAKBEGINCONSERTA<' + tag + '>BREAKBEGINCONSERTA').replace('</' + tag + '>', 'BREAKBEGINCONSERTA</' + tag + '>BREAKBEGINCONSERTA')
+        if self.content != rcontent:
+            parts = rcontent.split('BREAKBEGINCONSERTA')
+            self.content = self._fix_problem(tag_list, parts)
+        for tag in tags:
+            self.content = self.content.replace('</' + tag + '><' + tag + '>', '')
+
+    def _fix_problem(self, tag_list, parts):
+        expected_close_tags = []
+        ign_list = []
+        debug = False
+        k = 0
+        for part in parts:
+            if part in tag_list:
+                tag = part
+                if debug:
+                    print('\ncurrent:' + tag)
+                if tag.startswith('</'):
+                    if debug:
+                        print('expected')
+                        print(expected_close_tags)
+                        print('ign_list')
+                        print(ign_list)
+                    if tag in ign_list:
+                        if debug:
+                            print('remove from ignore')
+                        ign_list.remove(tag)
+                        parts[k] = ''
+                    else:
+                        matched = False
+                        if len(expected_close_tags) > 0:
+                            matched = (expected_close_tags[-1] == tag)
+                            if not matched:
+                                if debug:
+                                    print('not matched')
+                                while not matched and len(expected_close_tags) > 0:
+                                    ign_list.append(expected_close_tags[-1])
+                                    parts[k-1] += expected_close_tags[-1]
+                                    del expected_close_tags[-1]
+                                    matched = (expected_close_tags[-1] == tag)
+                                if debug:
+                                    print('...expected')
+                                    print(expected_close_tags)
+                                    print('...ign_list')
+                                    print(ign_list)
+
+                            if matched:
+                                del expected_close_tags[-1]
+                else:
+                    expected_close_tags.append(tag.replace('<', '</'))
+            k += 1
+        return ''.join(parts)
+
+
+def rename_embedded_img_href(content, xml_name, new_href_list):
+    content = content.replace('<graphic href="?', '--FIXHREF--<graphic href="?')
+    items = content.split('--FIXHREF--')
+    new = ''
+    i = 0
+    for item in items:
+        if item.startswith('<graphic href="?'):
+            s = item[item.find('?'):]
+            new += '<graphic href="' + xml_name + new_href_list[i] + s[s.find('"'):]
+            i += 1
+        else:
+            new += item
+    return new
+
+
+def html_img_src(html_content):
+    #[graphic href=&quot;?a20_115&quot;]</span><img border=0 width=508 height=314
+    #src="a20_115.temp_arquivos/image001.jpg"><span style='color:#33CCCC'>[/graphic]
+    html_content = html_content.replace('[graphic href="?', '[graphic href="?' + '"--FIXHREF--FIXHREF')
+    items = [item for item in html_content.split('--FIXHREF--') if item.startswith('FIXHREF')]
+    img_src = []
+    for item in items:
+        if ' src="' in item:
+            item = item[item.find(' src="') + len(' src="')]
+            item = item[0:item.find('"')]
+            item = item[item.find('/') + 1:]
+            if len(item) > 0:
+                img_src.append(item)
+    return img_src
+
+
+def extract_embedded_images(xml_name, content, html_filename, dest_path):
+    if content.find('href="?' + xml_name):
+        html_content = open(html_filename, 'r').read()
+        embedded_img_files = html_img_src(html_content)
+        embedded_img_path = os.path.dirname(html_filename)
+        content = rename_embedded_img_href(content, xml_name, embedded_img_files)
+        for item in embedded_img_files:
+            if os.path.isfile(embedded_img_path + '/' + item):
+                shutil.copyfile(embedded_img_path + '/' + item, dest_path + '/' + xml_name + item)
+    return content
+
+
+def normalize_sgmlxml(xml_name, content, src_path, version, html_filename):
+    content = extract_embedded_images(xml_name, content, html_filename, src_path)
+    if not xml_utils.is_xml_well_formed(content):
+        content = fix_xml(content)
+    if xml_utils.is_xml_well_formed(content) is not None:
+        content = java_xml_utils.xml_content_transform(content, xsl_sgml2xml(version))
+    return content
+
+
+def fix_xml(content):
+    xml_fix = XMLContent(content)
+    xml_fix.fix()
+    if not xml_fix.content == content:
+        content = xml_fix.content
+    return content
 
 
 def hdimages_to_jpeg(source_path, jpg_path, replace=False):
@@ -288,7 +497,7 @@ def generate_article_xml_package(xml_filename, scielo_pkg_path, report_path, wrk
     err_filename = report_path + '/' + xml_name + '.err.txt'
 
     clean_folder(xml_wrk_path)
-    delete_files([log_filename, err_filename])
+    delete_files([err_filename])
 
     is_sgmxml = xml_filename.endswith('.sgm.xml')
     html_filename = ''
@@ -300,7 +509,7 @@ def generate_article_xml_package(xml_filename, scielo_pkg_path, report_path, wrk
         html_filename = xml_wrk_path + '/' + xml_name + '.temp.htm'
         if not os.path.isfile(html_filename):
             html_filename += 'l'
-        content = sgml2xml.normalize_sgmlxml(xml_name, content, xml_path, version, html_filename)
+        content = normalize_sgmlxml(xml_name, content, xml_path, version, html_filename)
 
     if xml_utils.is_xml_well_formed(content) is None:
         new_xml_filename = scielo_pkg_path + '/incorrect_' + xml_name + '.xml'
@@ -317,7 +526,7 @@ def generate_article_xml_package(xml_filename, scielo_pkg_path, report_path, wrk
         if is_sgmxml:
             new_name = format_new_name(doc, acron, xml_name)
             curr_and_new_href_list = get_curr_and_new_href_list(xml_name, new_name, attach_info)
-            content = sgml2xml.normalize_href(content, curr_and_new_href_list)
+            content = normalize_hrefs(content, curr_and_new_href_list)
         else:
             curr_and_new_href_list = [(href, href) for href, attach_type, attach_id in attach_info]
         print(curr_and_new_href_list)
@@ -348,36 +557,60 @@ def generate_issue_xml_package(xml_files, scielo_pkg_path, report_path, wrk_path
     return reports
 
 
-def evaluate_article_xml_package(xml_name, new_name, scielo_pkg_path, pmc_validation_result, err_filename, ctrl_filename):
-    xsl_new_name = new_name if new_name != xml_name else ''
-    img_path = scielo_pkg_path
+def apply_dtd(content, dtd_filename, doctype):
+    xml_str = XMLContent(content)
+    xml_str.fix_dtd_location(dtd_filename, doctype)
+    return xml_str.content
+
+
+def apply_check_list(xml_filename, dtd_validation_report_filename, style_checker_report_filename):
+    def get_temp_filename(xml_filename):
+        temp_dir = tempfile.mkdtemp()
+        return temp_dir + '/' + os.path.basename(xml_filename)
+
+    #well_formed, is_dtd_valid, report_ok, preview_ok, output_ok = (False, False, False, False, False)
+    xml = xml_utils.is_xml_well_formed(xml_filename)
+    if xml:
+        version = xml.find('.').attrib.get('dtd-version', DEFAULT_VERSION)
+
+        content = open(xml_filename, 'r').read()
+        content = apply_dtd(dtd_filename(version), doctype(version))
+
+        temp_filename = get_temp_filename(xml_filename)
+        open(temp_filename, 'w').write(content)
+
+        is_valid_dtd = xpchecker.dtd_validation(temp_filename, dtd_validation_report_filename)
+        is_valid_style = xpchecker.style_validation(temp_filename, style_checker_report_filename, xsl_prep_report(version), xsl_report(version))
+
+        os.unlink(temp_filename)
+        shutil.rmtree(os.path.dirname(temp_filename))
+
+    return (xml, is_valid_dtd, is_valid_style)
+
+
+def evaluate_article_xml_package(xml_name, new_name, scielo_pkg_path, report_path):
     xml_filename = scielo_pkg_path + '/' + new_name + '.xml'
+    report_content = ''
+    dtd_validation_report_filename = report_path + '/' + xml_name + '.dtd.txt'
+    style_checker_report_filename = report_path + '/' + xml_name + '.rep.html'
+    delete_files([dtd_validation_report_filename, style_checker_report_filename])
 
     if os.path.isfile(xml_filename):
-        if xml_utils.is_xml_well_formed(xml_filename) is not None:
-            scielo_validation_result = self.sci_validator.check_list(xml_filename, scielo_validation_result, img_path)
-            scielo_validation_result.manage_result(ctrl_filename)
+        xml, is_valid_dtd, is_valid_style = apply_check_list(xml_filename, dtd_validation_report_filename, style_checker_report_filename)
+        if xml is None:
+            report_content = 'XML is not well formed: ' + xml_filename + '\nProblem to load XML file. See ' + scielo_pkg_path + '/incorrect_' + xml_name + '.xml'
+            open(dtd_validation_report_filename, 'w').write(report_content)
 
-            if os.path.exists(pmc_validation_result.pkg_path + '/' + new_name + '.xml'):
-                self.add_href_extensions(pmc_validation_result.pkg_path + '/' + new_name + '.xml')
-                pmc_validation_result = self.pmc_validator.check_list(pmc_validation_result.pkg_path + '/' + new_name + '.xml', pmc_validation_result, img_path, xsl_new_name)
 
-                if ctrl_filename is None:
-                    pmc_validation_result.manage_result(None)
-
-                if os.path.exists(pmc_validation_result.xml_output):
-                    #self.add_href_extensions(pmc_validation_result.xml_output)
-                    print('  Finished')
-                else:
-                    print('\nUnable to create ' + pmc_validation_result.xml_output)
-                    log_message(err_filename, 'Unable to create ' + pmc_validation_result.xml_output)
-            else:
-                print('Unable to create ' + pmc_validation_result.pkg_path + '/' + new_name + '.xml')
-                log_message(err_filename, 'Unable to create ' + pmc_validation_result.pkg_path + '/' + new_name + '.xml')
+def responde_to_markup(ctrl_filename, dtd_validation_report_filename):
+    if ctrl_filename:
+        import time
+        stop_after = 60 * 5
+        start = time.time()
+        doit = True
+        while doit and not os.path.isfile(dtd_validation_report_filename):
+            doit = ((time.time() - start) < stop_after)
+        if os.path.isfile(dtd_validation_report_filename):
+            open(ctrl_filename, 'w').write('done')
         else:
-            print('XML is not well formed: ' + xml_filename)
-            log_message(err_filename, 'XML is not well formed: ' + xml_filename)
-    else:
-        print('Problem to load XML file. See ' + scielo_pkg_path + '/incorrect_' + new_name + '.xml')
-        log_message(err_filename, 'Problem to load XML file. See ' + scielo_pkg_path + '/incorrect_' + new_name + '.xml')
-
+            open(ctrl_filename, 'w').write('interrupted')
