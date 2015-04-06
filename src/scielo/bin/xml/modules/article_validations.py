d1bf1d700375efe9f76f322bc740fd6c67c3266e
Roberta Takenaka <roberta.takenaka@scielo.org>
2015-04-06 15:24:48 -0300

    Fixes #1302

--- src/scielo/bin/xml/modules/article_validations.py
+++ src/scielo/bin/xml/modules/article_validations.py
@@ -736,7 +736,7 @@ class ReferenceContentValidation(object):
         if self.reference.ref_status == 'display-only':
             found_fatal = list(set([status for label, status, message if status in ['FATAL ERROR']]))
             if len(found_fatal) == 0:
-                r.append('@specific-use', 'FATAL ERROR', 'Remove @specific-use="display-only"')
+                r.append('@specific-use', 'FATAL ERROR', 'Remove @specific-use="display-only". It must be used only if reference is incomplete.')
             else:
                 items = []
                 for label, status, message in r:
