c8f5780b8ea06ef39f532dd7137b86965f74b77b
Roberta Takenaka <roberta.takenaka@scielo.org>
2015-04-09 17:33:28 -0300

    Adiciona arquivos renomeados

---
+++ src/scielo/bin/xml/modules/html_reports_collapsible.js
@@ -0,0 +1,21 @@
+<script language="JavaScript" type="text/javascript">
+<!--
+
+function openClose(theID) {
+if (document.getElementById(theID).style.display == "none") { 
+    document.getElementById(theID).style.display = "block" 
+    document.getElementById('hide'+theID).style.display = "block" 
+    document.getElementById('show'+theID).style.display = "none" 
+}
+else { 
+    document.getElementById(theID).style.display = "none" 
+    document.getElementById('hide'+theID).style.display = "none" 
+    document.getElementById('show'+theID).style.display = "block" 
+    window.location = '#begin_' + theID;
+    
+} }
+// -->
+</script>
+
+
+<!-- Read more : http://www.ehow.com/how_5772308_expand-collapse-text.html -->
