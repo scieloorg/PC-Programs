5c09edbf593002686e7349da945e833bd7b627cc
Roberta Takenaka <roberta.takenaka@scielo.org>
2016-04-01 11:40:40 -0300

    Novo código de país: Kosovo=KV
    Fixes #2188

--- src/scielo/bin/xml/modules/attributes.py
+++ src/scielo/bin/xml/modules/attributes.py
@@ -244,6 +244,7 @@ COUNTRY_CODES = [
  'HN', 'HR', 'HT', 'HU', 'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IR', 
  'IS', 'IT', 'JE', 'JM', 'JO', 'JP', 'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 
  'KP', 'KR', 'KW', 'KY', 'KZ', 'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 
+ 'KV',
  'LT', 'LU', 'LV', 'LY', 'MA', 'MC', 'MD', 'ME', 'MF', 'MG', 'MH', 'MK', 
  'ML', 'MM', 'MN', 'MO', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MU', 'MV', 'MW', 
  'MX', 'MY', 'MZ', 'NA', 'NC', 'NE', 'NF', 'NG', 'NI', 'NL', 'NO', 'NP', 
