b598f0c81272ac25378efa9ebf5452950b3bbff4
robertatakenaka <roberta.takenaka@scielo.org>
2017-08-03 18:14:55 -0300

    Ajustes

---
+++ src/scielo/bin/xml/modules/app/server/filestransfer.py
@@ -0,0 +1,62 @@
+# coding=utf-8
+
+import logging
+
+from ...generics import system
+
+
+#FIXME Makeme generics
+class FilesTransfer(object):
+
+    def __init__(self, config, log_filename=None):
+        self.config = config
+        self.log_filename = log_filename
+        if log_filename is not None:
+            logging.basicConfig(log_filename, format='%(asctime)s %(message)s')
+
+    def transfer_website_files(self, acron, issue_id):
+        if self.config.is_enabled_transference:
+            issue_id_path = acron + '/' + issue_id
+            folders = ['/htdocs/img/revistas/', '/bases/pdf/', '/bases/xml/']
+            for folder in folders:
+                dest_path = self.config.remote_web_app_path + folder + issue_id_path
+                source_path = self.config.local_web_app_path + folder + issue_id_path
+                for server in self.config.transference_servers:
+                    self.run_remote_mkdirs(server, dest_path)
+                    self.run_rsync(source_path, server, dest_path)
+
+    def transfer_report_files(self, acron, issue_id):
+        # 'rsync -CrvK img/* self.config.user@server:/var/www/...../revistas'
+        if self.config.is_enabled_transference:
+            issue_id_path = acron + '/' + issue_id
+            folders = ['/htdocs/reports/']
+            for folder in folders:
+                dest_path = self.config.remote_web_app_path + folder + issue_id_path
+                source_path = self.config.local_web_app_path + folder + issue_id_path
+                log_filename = './transfer_report_' + issue_id_path.replace('/', '-') + '.log'
+                for server in self.config.transference_servers:
+                    self.run_remote_mkdirs(server, dest_path, log_filename)
+                    self.run_rsync(source_path, server, dest_path, log_filename)
+
+    def run_cmd(self, cmd):
+        if self.log_filename is not None:
+            logging.info(cmd)
+        try:
+            system.run_command(cmd)
+            if self.log_filename is not None:
+                logging.info('done')
+        except:
+            if self.log_filename is not None:
+                logging.error('failure')
+
+    def run_remote_mkdirs(self, server, path):
+        cmd = 'ssh ' + self.config.user + '@' + server + ' "mkdir -p ' + path + '"'
+        self.run_cmd(cmd)
+
+    def run_rsync(self, source, server, dest):
+        cmd = 'nohup rsync -CrvK ' + source + '/* ' + self.config.user + '@' + server + ':' + dest + '&\n'
+        self.run_cmd(cmd)
+
+    def run_scp(self, source, server, dest):
+        cmd = 'nohup scp -r ' + source + ' ' + self.config.user + '@' + server + ':' + dest + '&\n'
+        self.run_cmd(cmd)
---
+++ src/scielo/bin/xml/modules/app/server/mailer.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+
+from ...generics import email_service
+
+
+class Mailer(object):
+
+    def __init__(self, config):
+        self.config = config
+        self.mailer = None
+        print('Mailer', config.is_enabled_email_service, config.email_sender_name)
+        if config.is_enabled_email_service:
+            self.mailer = email_service.EmailService(config.email_sender_name, config.email_sender_email)
+
+    def send_message(self, to, subject, text, attaches):
+        self.mailer.send_message(to, subject, text, attaches)
+
+    def mail_invalid_packages(self, invalid_pkg_files):
+        if self.config.is_enabled_email_service:
+            self.send_message(self.config.email_to, self.config.email_subject_invalid_packages, self.config.email_text_invalid_packages + '\n'.join(invalid_pkg_files))
+
+    def mail_step1_failure(self, package_folder, e):
+        print('mail_step1', package_folder, self.config.is_enabled_email_service)
+        if self.config.is_enabled_email_service:
+            self.send_message(self.config.email_to_adm, '[Step 1]' + self.config.email_subject_invalid_packages, self.config.email_text_invalid_packages + '\n' + package_folder + '\n' + str(e))
+
+    def mail_results(self, package_folder, results, report_location):
+        if self.config.is_enabled_email_service:
+            self.send_message(self.config.email_to, self.config.email_subject_package_evaluation + u' ' + package_folder + u': ' + results, report_location)
+
+    def mail_step2_failure(self, package_folder, e):
+        if self.config.is_enabled_email_service:
+            self.send_message(self.config.email_to_adm, '[Step 2]' + self.config.email_subject_invalid_packages, self.config.email_text_invalid_packages + '\n' + package_folder + '\n' + str(e))
+
+    def mail_step3_failure(self, package_folder, e):
+        if self.config.is_enabled_email_service:
+            self.send_message(self.config.email_to_adm, '[Step 3]' + self.config.email_subject_invalid_packages, self.config.email_text_invalid_packages + '\n' + package_folder + '\n' + str(e))
