22f752ecd0bc3b68a691657fd889e0835cf48af8
robertatakenaka <roberta.takenaka@scielo.org>
2017-09-29 09:37:36 -0300

    40094_ajustes_mensagens

--- src/scielo/bin/xml/app_modules/generics/system.py
+++ src/scielo/bin/xml/app_modules/generics/system.py
@@ -12,8 +12,16 @@ def format_command(command, params=None):
     return command + ' ' + parameters
 
 
-def run_command(command):
+def run_command(command, display=False):
+    if display is True:
+        try:
+            encoding.display_message(u'Running:\n {}'.format(command))
+        except Exception as e:
+            pass
+
     try:
         os.system(encoding.encode(command, encoding.SYS_DEFAULT_ENCODING))
+        if display is True:
+            encoding.display_message('...done')
     except Exception as e:
         encoding.report_exception('system.run_command()', e, command)
