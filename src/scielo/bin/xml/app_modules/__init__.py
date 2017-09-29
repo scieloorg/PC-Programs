f9947489dc669318022c36f275e936356572f65e
robertatakenaka <roberta.takenaka@scielo.org>
2017-09-29 13:19:35 -0300

    40094_venv

--- src/scielo/bin/xml/app_modules/__init__.py
+++ src/scielo/bin/xml/app_modules/__init__.py
@@ -1,8 +1,14 @@
 # coding=utf-8
 import os
 
-from .app.config import app_texts
-from .app.config import app_caller
+try:
+    from app_modules.app.config import app_texts
+    from app_modules.app.config import app_caller
+    from app_modules.generics import logger
+except:
+    from .app.config import app_texts
+    from .app.config import app_caller
+    from .generics import logger
 
 
 THIS_LOCATION = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')
@@ -13,13 +19,14 @@ TMP_DIR = BIN_PATH + '/tmp'
 LOG_PATH = BIN_PATH + '/logs'
 PMC_PATH = BIN_PATH + '/pmc'
 BIN_MARKUP_PATH = BIN_PATH + '/markup'
-XC_SERVER_CONFIG_PATH = BIN_XML_PATH + '/config'
+XC_SERVER_CONFIG_PATH = BIN_PATH + '/config'
 VENV_PATH = BIN_XML_PATH + '/app_data/venv/scielo-programs'
 TABLES_PATH = BIN_XML_PATH + '/app_modules/settings/tables'
 LOCALE_PATH = BIN_XML_PATH + '/app_modules/settings/locale'
 FST_PATH = BIN_XML_PATH + '/app_modules/settings/fst'
 EMAIL_TEMPLATE_MESSAGES_PATH = BIN_XML_PATH + '/app_modules/settings/email'
 REQUIREMENTS_FILE = BIN_XML_PATH + '/app_modules/settings/requirements.txt'
+REQUIREMENTS_FILE_SPECIAL = BIN_XML_PATH + '/app_modules/settings/requirements-w64.txt'
 REQUIREMENTS_CHECKER = BIN_XML_PATH + '/app_modules/tools/requirements_checker.py'
 HTML_REPORTS_PATH = BIN_XML_PATH + '/app_modules/generics/reports/'
 
@@ -32,5 +39,11 @@ if not os.path.isdir(LOG_PATH):
 
 _ = app_texts.get_texts(LOCALE_PATH)
 
+try:
+    os.unlink(LOG_PATH+'/app_caller.log')
+except:
+    pass
 
-appcaller = app_caller.AppCaller(VENV_PATH)
+appcaller = app_caller.AppCaller(
+    logger.get_logger(LOG_PATH+'/app_caller.log', 'Environment'),
+    VENV_PATH)
--- src/scielo/bin/xml/app_modules/app/config/app_caller.py
+++ src/scielo/bin/xml/app_modules/app/config/app_caller.py
@@ -1,17 +1,22 @@
 
 import os
 import platform
+
 from ...generics import system
+from ...generics import fs_utils
 from ...generics import encoding
+from ...generics.ws import ws_requester
 
 
-so = platform.platform().lower()
+so, node, release, version, machine, processor = platform.uname()
+python_version = platform.python_version()
+so = so.lower()
 
 
 def info(venv_path):
     if 'windows' in so:
-        activate = '{}/Scripts/activate'.format(venv_path)
-        deactivate = '{}/Scripts/deactivate'.format(venv_path)
+        activate = '{}/Scripts/activate.bat'.format(venv_path)
+        deactivate = '{}/Scripts/deactivate.bat'.format(venv_path)
         sep = ' & '
     else:
         activate = 'source {}/bin/activate'.format(venv_path)
@@ -25,38 +30,58 @@ def info(venv_path):
 
 class AppCaller(object):
 
-    def __init__(self, venv_path=None):
+    def __init__(self, logger, venv_path=None):
         self.venv_path = venv_path
         self.activate_command, self.deactivate_command, self.sep = info(venv_path)
+        self.logger = logger
 
-    def install(self, requirements_file, requirements_checker):
+    def install_virtualenv(self):
         if self.venv_path is not None:
             if not os.path.isdir(self.venv_path):
-                commands = []
-                commands.append('pip install virtualenv')
-                commands.append(u'virtualenv {}'.format(self.venv_path))
-                self.execute(commands)
-        self.install_requirements(
-            requirements_file,
-            requirements_checker)
+                system.run_command('python -m pip install --upgrade pip', True)
+                system.run_command('pip install virtualenv', True)
+                system.run_command(u'virtualenv {}'.format(self.venv_path), True)
 
     def execute(self, commands):
-        _commands = []
-        encoding.debugging('app_caller.execute()', commands)
-        if os.path.isdir(self.venv_path):
-            _commands.append(u'echo Activating {}'.format(self.venv_path))
-            _commands.append(self.activate_command)
-            _commands.append('python -V')
+        _commands = [self.activate_command]
         _commands.extend(commands)
-        if os.path.isdir(self.venv_path):
-            _commands.append(self.deactivate_command)
-            _commands.append(u'echo Deactivating {}'.format(self.venv_path))
-        _commands = [item for item in _commands if len(item) > 0]
-        encoding.debugging('app_caller.execute()', self.sep.join(_commands))
-        system.run_command(self.sep.join(_commands))
+        _commands.append(self.deactivate_command)
+        self.execute_inline(_commands)
 
-    def install_requirements(self, requirements_file, requirements_checker):
+    def execute_inline(self, commands):
+        _commands = [item for item in commands if len(item) > 0]
+        cmd = self.sep.join(_commands)
+        if 'windows' in so:
+            cmd = cmd.replace('/', '\\')
+        self.logger.info(cmd)
+        system.run_command(cmd)
+
+    def install_requirements(self, requirements_file, requirements_checker, uninstall=True):
+        if uninstall is True:
+            self.uninstall_requirements(requirements_file)
         commands = []
+        commands.append('pip freeze')
         commands.append('pip install -r {}'.format(requirements_file))
         commands.append('python {}'.format(requirements_checker))
+        commands.append('pip freeze')
         self.execute(commands)
+
+    def uninstall_requirements(self, requirements_file):
+        commands = []
+        commands.append('pip freeze')
+        commands.append('pip uninstall -r {} -y'.format(requirements_file))
+        commands.append('pip freeze')
+        self.execute(commands)
+
+    def install_special_requirements(self, special_requirements, tmp_dir):
+        if os.path.isfile(special_requirements):
+            for url in fs_utils.read_file_lines(special_requirements):
+                filename = url[url.rfind('/')+1:]
+                filename = filename[:filename.find('#')]
+                filename = tmp_dir+'/'+filename
+                ws_requester.urllib_request.urlretrieve(
+                    url, filename=filename)
+                self.execute(['pip install {}'.format(filename)])
+        else:
+            encoding.display_message(
+                _('Not found {}. '.format(special_requirements)))
