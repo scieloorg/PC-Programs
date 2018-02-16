912917cd1e1c019861cddee3fa25824e57ff2247
robertatakenaka <roberta.takenaka@scielo.org>
2018-02-16 13:44:05 -0200

    40095_install

--- src/scielo/bin/xml/run_app.py
+++ src/scielo/bin/xml/run_app.py
@@ -1,11 +1,8 @@
-
 import sys
 
 from app_modules.generics import system
 from app_modules.__init__ import appcaller
 from app_modules.__init__ import BIN_XML_PATH
-
-from app_modules.app.config import app_caller
 
 
 def execute(parameters):
@@ -14,24 +11,45 @@ def execute(parameters):
             'python {}/run_app.py'.format(BIN_XML_PATH), parameters)])
 
 
-if __name__ == '__main__':
-    argv = sys.argv[1:]
-    if sys.argv[1] == 'install':
-        from app_modules.__init__ import REQUIREMENTS_FILE
-        from app_modules.__init__ import REQUIREMENTS_CHECKER
-        from app_modules.__init__ import REQUIREMENTS_FILE_SPECIAL
-        from app_modules.__init__ import TMP_DIR
+def requirements_checker():
+    required = []
+    try:
+        import PIL
+    except:
+        required.append('PIL')
+    try:
+        import packtools
+    except:
+        required.append('packtools')
+    return required
+
+
+def check_requirements():
+    reqs = requirements_checker()
+    if len(reqs) > 0:
         appcaller.install_virtualenv()
-        appcaller.install_requirements(REQUIREMENTS_FILE, REQUIREMENTS_CHECKER)
-        # if app_caller.so == 'windows' and app_caller.machine != 'x86':
-        #     appcaller.install_special_requirements(REQUIREMENTS_FILE_SPECIAL, TMP_DIR)
-    elif sys.argv[1].endswith('xml_converter.py'):
-        from app_modules.app import xc
-        xc.call_converter(argv, '1.1')
-    elif sys.argv[1].endswith('xml_package_maker.py'):
+        appcaller.install_requirements()
+
+
+def main(parameters):
+    argv = parameters[1:]
+
+    if parameters[1].endswith('xml_package_maker.py'):
+        check_requirements()
         from app_modules.app import xpm
         xpm.call_make_packages(argv, '1.1')
+    elif parameters[1].endswith('xml_converter.py'):
+        check_requirements()
+        from app_modules.app import xc
+        xc.call_converter(argv, '1.1')
+    elif parameters[1] == 'install':
+        appcaller.install_virtualenv(True)
+        appcaller.install_requirements()
     else:
         print('unknown command')
-        print(sys.argv)
+        print(parameters)
         print('do nothing')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
