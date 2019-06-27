a2b6f096f30a029c0e6fda2bbd64a97c04ab5101
Roberta Takenaka <roberta.takenaka@scielo.org>
2019-06-27 09:29:13 -0300

    Ajuste para funcionar em python 2.7

--- src/scielo/bin/xml/app_modules/app/data/scielo_id_gen.py
+++ src/scielo/bin/xml/app_modules/app/data/scielo_id_gen.py
@@ -1,4 +1,13 @@
-from math import log2, ceil
+from math import ceil
+try:
+    from math import log2
+except ImportError as e:
+    from math import log
+
+    def log2(x):
+        return log(x, 2)
+
+
 from uuid import UUID, uuid4
 
 
@@ -9,7 +18,7 @@ chars_map = {dig: idx for idx, dig in enumerate(digit_chars)}
 def uuid2str(value):
     result = []
     unevaluated = value.int
-    for unused in range(ceil(128 / log2(len(digit_chars)))):
+    for unused in range(int(ceil(128 / log2(len(digit_chars))))):
         unevaluated, remainder = divmod(unevaluated, len(digit_chars))
         result.append(digit_chars[remainder])
     return "".join(result)
