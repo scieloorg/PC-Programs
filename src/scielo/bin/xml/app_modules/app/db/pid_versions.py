693ad16b55d5d363b3637c8bcc37e6d1c2561016
Joffily Ferreira <joffily.ferreira@scielo.org>
2020-01-17 16:53:12 -0300

    Adiciona interface de acesso ao banco de PIDs
    Este commit adiciona os códigos que dão acesso a interface de acessso
    ao banco de dados SQLite. Banco este que armazena os PIDs (v2 e v3)
    dos artigos já processados pelo XC.
    
    A utilização do SQLite facilita a instalação do XC sem a necessidade de
    utilizar bancos instaláveis como o PostgreSQL e MySQL.

--- src/scielo/bin/xml/app_modules/app/db/pid_versions.py
+++ src/scielo/bin/xml/app_modules/app/db/pid_versions.py
@@ -1,70 +1,85 @@
 import sqlite3
+import logging
+
+CREATE_PID_TABLE_QUERY = """
+    CREATE TABLE IF NOT EXISTS pid_versions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        v2 VARCHAR(23) UNIQUE,
+        v3 VARCHAR(255) UNIQUE
+    );
+"""
 
 
 class PIDVersionsManager:
-
     def __init__(self, db):
         self.db = db
+        self.db.cursor.execute(CREATE_PID_TABLE_QUERY)
 
-    def insert(self, v2, v3):
-        self.db.insert(v2, v3)
+    def register(self, v2, v3):
+        return self.db.insert("INSERT INTO pid_versions (v2, v3) VALUES (?,?)", (v2, v3,))
 
     def get_pid_v3(self, v2):
         return self.db.get_pid_v3(v2)
 
-
-class PIDVersionsBD:
-
-    def __init__(self, db_name):
-        self.db_name = db_name
-        self._connection = None
-        self._cursor = None
-        self._is_table_created = None
-
-    @property
-    def conn(self):
-        if self._connection is None:
-            self._connection = sqlite3.connect(self.db_name)
-        return self._connection
-
-    @property
-    def cursor(self):
-        if self._cursor is None:
-            self._cursor = self.conn.cursor()
-        return self._cursor
-
-    @property
-    def is_table_created(self):
-        if self._is_table_created is None:
-            self._is_table_created = self.create_table()
-        return self._is_table_created
-
-    def _execute(self, command, parameters=None):
-        if self._is_table_created:
-            if parameters:
-                self.cursor.execute(command, parameters)
-            else:
-                self.cursor.execute(command)
-            if command.startswith("SELECT"):
-                return self.cursor.fetchone()
-            else:
-                self.conn.commit()
-                self.conn.close()
-                return True
-
-    def create_table(self):
-        return self._execute(
-            """CREATE TABLE IF NOT EXISTS pid_versions (v2 varchar(23) unique, v3 varchar(255) unique)"""
+    def pids_already_registered(self, v2, v3):
+        """Verifica se a chave composta (v2 e v3) existe no banco de dadoss"""
+        result = self.db.fetch(
+            "SELECT COUNT(*) FROM pid_versions WHERE v2 = ? and v3 = ?", (v2, v3,)
         )
+        return result[0][0] == 1
 
-    def insert(self, v2, v3):
-        return self._execute(
-            """INSERT INTO pid_versions VALUES ('?','?')""", (v2, v3)
-        )
+    def close(self):
+        self.db.close()
+
+
+class PIDVersionsDB:
+    def __init__(self, name):
+        self.conn = None
+        self.cursor = None
+        self.open(name)
+
+    def open(self, name):
+        try:
+            self.conn = sqlite3.connect(name)
+            self.cursor = self.conn.cursor()
+        except sqlite3.OperationalError as e:
+            logging.exception(e)
+            raise sqlite3.OperationalError("unable to open database '%s'" % name)
+
+    def close(self):
+        if self.conn is not None:
+            self.conn.commit()
+            self.conn.close()
+            self.cursor.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, excution_type, excution_value, traceback):
+
+        if isinstance(excution_value, Exception):
+            self.conn.rollback()
+        else:
+            self.conn.commit()
+
+        self.close()
+
+    def fetch(self, sql, parameters=None):
+        self.cursor.execute(sql, parameters)
+        return self.cursor.fetchall()
+
+    def insert(self, sql, parameters):
+        try:
+            self.cursor.execute(sql, parameters)
+        except sqlite3.IntegrityError as e:
+            logging.error("this item already exists in database")
+            return False
+        else:
+            self.conn.commit()
+            return True
 
     def get_pid_v3(self, v2):
-        expr = (v2, )
-        found = self._execute(
-            """SELECT v3 FROM pid_versions WHERE v2 = ?""", expr)
-        if found:
-            return found[0]
+        found = self.fetch("SELECT v3 FROM pid_versions WHERE v2 = ?", (v2,))
+
+        if found is not None and len(found) > 0:
+            return found[0][0]
