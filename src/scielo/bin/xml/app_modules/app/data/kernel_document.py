65256a3210251106247e57e140b8f10f5e143fee
Joffily Ferreira <joffily.ferreira@scielo.org>
2020-01-17 16:59:35 -0300

    Adiciona a recuperação do pid v3 a partir da base SQLite
    Este commit permite que o processamento dos artigos recupere a versão 3
    do pid a partir de uma base SQLite previamente populada.
    
    O código também é capaz de gerar um novo PID para os casos em que o
    artigo não possua o mesmo.
    
    As situações de recuperação e geração de pid são:
    1) Os pids são recuperados a partir do próprio XML;
    2) Os pids são extraídos das bases MST (`documents_in_isis`);
    3) O pid v2 é calculado pela regra corrente (legado);
    4) O pid v3 é recuperado a partir da base SQLite;

--- src/scielo/bin/xml/app_modules/app/data/kernel_document.py
+++ src/scielo/bin/xml/app_modules/app/data/kernel_document.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from ...generics import fs_utils
 from . import scielo_id_gen
 
@@ -5,56 +6,50 @@ import xml.etree.ElementTree as ET
 
 
 def add_article_id_to_received_documents(
-    pid_manager,
-    issn_id, year_and_order, received_docs, registered_docs, file_paths):
+    pid_manager, issn_id, year_and_order, received_docs, documents_in_isis, file_paths
+):
+    """Atualiza article-id (scielo-v2 e scielo-v3) dos documentos recebidos.
+
+    Params:
+        pid_manager (PIDVersionsManager): instância de PIDVersionsManager para gerir pid da versão 3
+        issn_id (str): ISSN do periódico
+        year_and_order (str): Ano e ordem da issue processada
+        received_docs (dict): Pacote de documentos recebidos para processar
+        documents_in_isis (dict): Documentos já registrados na base isis (acron/volnum)
+        file_paths (dict): arquivos do received_docs
+
+    Returns:
+        None
     """
-    Atualiza article-id (scielo-v2 e scielo-v3) dos documentos recebidos.
-    pid_manager: instância de PIDVersionsManager para gerir pid da versão 3
-    issn_id: ISSN ID, chave para gerar o pid da versão 2
-    year_and_order: também faz parte do pid da versão 2
-    received_docs: pacote de documentos recebidos para processar
-    registered_docs: documentos já registrados na base isis (acron/volnum)
-    file_paths: arquivos do received_docs
-    """
-    for name, received in received_docs.items():
-        pid_v2 = received.get_scielo_pid("v2")
-        pid_v3 = received.get_scielo_pid("v3")
+
+    for xml_name, article in received_docs.items():
+        pid_v2 = article.get_scielo_pid("v2")
+        pid_v3 = article.get_scielo_pid("v3")
+        pids_to_append_in_xml = []
+
         if pid_v2 and pid_v3:
-            """
-            TODO: validar o pid_v3 que está presente no pacote recebido
-            e / ou registrá-lo caso não esteja registrado
-            """
+            exists_in_database = pid_manager.pids_already_registered(pid_v2, pid_v3)
+
+            if not exists_in_database:
+                pid_manager.register(pid_v2, pid_v3)
+
             continue
 
-        pid_and_specific_use_items = []
+        if pid_v2 is None:
+            pid_v2 = get_scielo_pid_v2(issn_id, year_and_order, article.order)
+            pids_to_append_in_xml.append((pid_v2, "scielo-v2"))
 
-        if not pid_v2:
-            pid_v2 = get_scielo_pid_v2(issn_id, year_and_order, received.order)
-            pid_and_specific_use_items.append((pid_v2, "scielo-v2"))
+        if pid_v3 is None:
+            pid_v3 = (
+                documents_in_isis.get(xml_name, article).scielo_id
+                or pid_manager.get_pid_v3(pid_v2)
+                or scielo_id_gen.generate_scielo_pid()
+            )
+            pid_manager.register(pid_v2, pid_v3)
+            article.registered_scielo_id = pid_v3
+            pids_to_append_in_xml.append((pid_v3, "scielo-v3"))
 
-        if not pid_v3:
-            pid_v3_from_manager = None
-            registered = registered_docs.get(name)
-            if registered and registered.scielo_id:
-                pid_v3 = registered.scielo_id
-
-            if not pid_v3 and pid_manager:
-                pid_v3 = pid_manager.get_pid_v3(pid_v2)
-                pid_v3_from_manager = pid_v3
-
-            if not pid_v3:
-                pid_v3 = scielo_id_gen.generate_scielo_pid()
-
-            received.registered_scielo_id = pid_v3
-            if pid_v3_from_manager is None:
-                pid_manager.insert(pid_v2, pid_v3)
-
-            pid_and_specific_use_items.append((pid_v3, "scielo-v3"))
-
-        if pid_and_specific_use_items:
-            add_article_id_to_xml(
-                file_paths.get(name),
-                pid_and_specific_use_items)
+        add_article_id_to_xml(file_paths.get(xml_name), pids_to_append_in_xml)
 
 
 def get_scielo_pid_v2(issn_id, year_and_order, order_in_issue):
@@ -72,6 +67,6 @@ def add_article_id_to_xml(file_path, pid_and_specific_use_items):
             article_id.text = id_value
             article_id.set("specific-use", specific_use)
             article_id.set("pub-id-type", "publisher-id")
-            article_meta.append(article_id)
+            article_meta.insert(0, article_id)
         new_content = ET.tostring(xml.find(".")).decode("utf-8")
         fs_utils.write_file(file_path, new_content)
