7c33aa408d82415db30b16ecc6c096384150e07a
Roberta Takenaka <roberta.takenaka@scielo.org>
2020-05-26 16:20:00 -0300

    Ajustes para que os módulos possam ser executados na forma: python *.py, sem dependência com o setup.py
    Desta forma, voltam a funcionar:
    python xml_package_maker.py
    python xml_converter.py
    python xml_pubmed.py
    python download_markup_journals.py

--- src/scielo/bin/xml/prodtools/__init__.py
+++ src/scielo/bin/xml/prodtools/__init__.py
@@ -8,22 +8,21 @@ path_join = os.path.join
 
 PRODTOOLS_PATH = os.path.dirname(os.path.realpath(__file__))
 
-TWO_PARENTS_UPPER = os.path.dirname(os.path.dirname(PRODTOOLS_PATH))
+CURRENT_DIR_PARENT_PATH = os.path.dirname(os.getcwd())
+CURRENT_NAME = os.path.basename(os.getcwd())
+print(CURRENT_DIR_PARENT_PATH)
+print(CURRENT_NAME)
 
-BIN_PATH = ''
-SERVER_WORKSPACE_PATH = ''
-if os.path.basename(TWO_PARENTS_UPPER) == "bin":
-    BIN_PATH = TWO_PARENTS_UPPER
-    BIN_MARKUP_PATH = path_join(BIN_PATH, 'markup')
-else:
-    SERVER_WORKSPACE_PATH = os.getcwd()
-    XC_SERVER_CONFIG_PATH = path_join(SERVER_WORKSPACE_PATH, 'config')
-    LOG_PATH = path_join(SERVER_WORKSPACE_PATH, 'logs')
-    BIN_MARKUP_PATH = path_join(SERVER_WORKSPACE_PATH, 'markup')
-    if not os.path.isdir(LOG_PATH):
-        os.makedirs(LOG_PATH)
-    if not os.path.isdir(BIN_MARKUP_PATH):
-        os.makedirs(BIN_MARKUP_PATH)
+BIN_PATH = CURRENT_DIR_PARENT_PATH
+BIN_MARKUP_PATH = path_join(BIN_PATH, 'markup')
+
+XC_SERVER_CONFIG_PATH = path_join(CURRENT_DIR_PARENT_PATH, 'config')
+LOG_PATH = path_join(CURRENT_DIR_PARENT_PATH, 'logs')
+
+if not os.path.isdir(LOG_PATH):
+    os.makedirs(LOG_PATH)
+if not os.path.isdir(BIN_MARKUP_PATH):
+    os.makedirs(BIN_MARKUP_PATH)
 
 TABLES_PATH = path_join(PRODTOOLS_PATH, 'settings', 'tables')
 LOCALE_PATH = path_join(PRODTOOLS_PATH, 'locale')
@@ -34,6 +33,6 @@ DTD_AND_XSL_PATH = path_join(PRODTOOLS_PATH, 'settings', 'dtd_and_xsl')
 
 XPM_VERSION_FILE_PATH = path_join(PRODTOOLS_PATH, 'xpm_version.txt')
 
-
+ICON = path_join(PRODTOOLS_PATH, 'settings', 'Scielo.ico')
 
 _ = app_texts.get_texts(LOCALE_PATH)
