8419a294dad95d54dbae3c6f36183b185af0f788
Roberta Takenaka <roberta.takenaka@scielo.org>
2020-06-09 16:43:20 -0300

    Move RegisteredIssue para xc_models.py Exclui registered.py

--- src/scielo/bin/xml/prodtools/db/xc_models.py
+++ src/scielo/bin/xml/prodtools/db/xc_models.py
@@ -1544,6 +1544,21 @@ def update_list(l, value):
     return l
 
 
+class RegisteredIssue(object):
+
+    def __init__(self):
+        self.articles_db_manager = None
+        self.issue_error_msg = None
+        self.issue_models = None
+        self.issue_files = None
+
+    @property
+    def registered_articles(self):
+        if self.articles_db_manager is not None:
+            return self.articles_db_manager.registered_articles
+        return {}
+
+
 class RegisteredIssuesManager(object):
 
     def __init__(self, config, is_db_generation):
