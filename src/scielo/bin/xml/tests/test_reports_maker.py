56b452a882ebf696dc69cdbd5c761be46f134016
Patricia Morimoto <patricia.morimoto@scielo.org>
2020-07-10 15:45:06 -0300

    Testes unitários para validar CollectionAssetsInReport.save_report()

--- src/scielo/bin/xml/tests/test_reports_maker.py
+++ src/scielo/bin/xml/tests/test_reports_maker.py
@@ -1,3 +1,6 @@
+import tempfile
+import shutil
+import pathlib
 from unittest import TestCase
 
 from prodtools.validations.reports_maker import (
@@ -315,3 +318,78 @@ class TestCollectionAssetsInReportForRemoteWebsite(TestCase):
         self.assertEqual(
             '/scielo/serial_path/acron/issue_label/base_xml/base_source',
             self.data.serial_base_xml_path)
+
+
+class TestCollectionAssetsInReportValidateSaveReport(TestCase):
+
+    def setUp(self):
+        self.serial_path = tempfile.mkdtemp()
+        self.web_app_path = tempfile.mkdtemp()
+        self.data = CollectionAssetsInReport(
+            "acron", "issue_label",
+            self.serial_path,
+            self.web_app_path,
+            "https://qa.scielo.br",
+        )
+
+    def tearDown(self):
+        self.serial_path = tempfile.mkdtemp()
+        self.web_app_path = tempfile.mkdtemp()
+
+    def test_report_path(self):
+        expected = str(
+            pathlib.Path(self.web_app_path) / 'htdocs/reports/acron/issue_label'
+        )
+        self.assertEqual(expected, self.data.report_path)
+
+    def test_serial_report_path(self):
+        expected = str(
+            pathlib.Path(self.serial_path) / 'acron/issue_label/base_xml/base_reports'
+        )
+        self.assertEqual(expected, self.data.serial_report_path)
+
+    def test_xml_path(self):
+        expected = str(
+            pathlib.Path(self.web_app_path) / 'bases/xml/acron/issue_label'
+        )
+        self.assertEqual(expected, self.data.xml_path)
+
+    def test_save_report_saves_in_serial_report_path(self):
+        report_text = "<html><body><p>Teste</p></body></html>"
+        with tempfile.TemporaryDirectory() as temp_dir_path:
+            report_path = pathlib.Path(temp_dir_path) / "report.html"
+            report_path.write_text(report_text)
+            self.data.save_report(str(report_path))
+            path_result = pathlib.Path(self.data.serial_report_path) / "report.html"
+            self.assertEqual(path_result.read_text(), report_text)
+
+    def test_save_report_without_converted_xml(self):
+        # Garante que dir report_path existe
+        pathlib.Path(self.data.report_path).mkdir(parents=True, exist_ok=True)
+        # Grava fake report
+        report_path = pathlib.Path(self.web_app_path) / "report.html"
+        report_path.write_text("<html><body><p>Teste</p></body></html>")
+        # Grava fake XML
+        temp_dir_path = pathlib.Path(self.data.xml_path)
+        temp_dir_path.mkdir(parents=True, exist_ok=True)
+
+        self.data.save_report(str(report_path))
+        path_result = pathlib.Path(self.data.report_path) / "doc.xml"
+        self.assertFalse(path_result.exists())
+
+    def test_save_report_copy_converted_xml_to_remote(self):
+        # Garante que dir report_path existe
+        pathlib.Path(self.data.report_path).mkdir(parents=True, exist_ok=True)
+        # Grava fake report
+        report_path = pathlib.Path(self.web_app_path) / "report.html"
+        report_path.write_text("<html><body><p>Teste</p></body></html>")
+        # Grava fake XML
+        xml_text = "<article><body><p>Teste</p></body></article>"
+        temp_dir_path = pathlib.Path(self.data.xml_path)
+        temp_dir_path.mkdir(parents=True, exist_ok=True)
+        xml_path = temp_dir_path / "doc.xml"
+        xml_path.write_text(xml_text)
+
+        self.data.save_report(str(report_path))
+        path_result = pathlib.Path(self.data.report_path) / "doc.xml"
+        self.assertEqual(path_result.read_text(), xml_text)
